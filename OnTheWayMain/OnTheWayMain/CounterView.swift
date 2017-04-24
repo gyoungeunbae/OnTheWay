903da7a6114c6311c0f8c83058e7de79442012d8
torpedo87 <bestofhand@naver.com>
2017-04-24 19:03:54 +0900

    Add healthkit coredata

---
+++ OnTheWayMain/OnTheWayMain/CounterView.swift
@@ -0,0 +1,76 @@
+//
+//  Draw2D.swift
+//  OnTheWayMain
+//
+//  Created by junwoo on 2017. 4. 22..
+//  Copyright © 2017년 junwoo. All rights reserved.
+//
+
+import UIKit
+
+//하루 목표 걸음수
+let stepOfGoal = 10000
+let π:CGFloat = CGFloat(M_PI)
+
+@IBDesignable class CounterView: UIView {
+    
+    @IBInspectable var stepOfWalked: Int = 0
+    @IBInspectable var outlineColor: UIColor = UIColor.blue
+    @IBInspectable var counterColor: UIColor = UIColor.yellow
+    
+    override func draw(_ rect: CGRect) {
+        
+        // 원의 중심
+        let center = CGPoint(x:bounds.width/2, y: bounds.height/2)
+        
+        // 반지름
+        let radius: CGFloat = max(bounds.width, bounds.height)
+        
+        // 두께
+        let arcWidth: CGFloat = 60
+        
+        // 시작각도, 마침각도
+        let startAngle: CGFloat = 3 * π / 4
+        let endAngle: CGFloat = π / 4
+        
+        // 회전경로 지정
+        var path = UIBezierPath(arcCenter: center,
+                                radius: radius/2 - arcWidth/2,
+                                startAngle: startAngle,
+                                endAngle: endAngle,
+                                clockwise: true)
+        
+        // 그리기
+        path.lineWidth = arcWidth
+        counterColor.setStroke()
+        path.stroke()
+        
+        
+        //Draw the outline
+        
+        //사용할 각도
+        let angleDifference: CGFloat = 2 * π - startAngle + endAngle
+        
+        //한 걸음당 각도
+        let anglePerMin = angleDifference / CGFloat(stepOfGoal)
+        
+        //오늘 걸은 각도
+        let outlineEndAngle = anglePerMin * CGFloat(stepOfWalked) + startAngle
+        
+        //경로 지정
+        var outlinePath = UIBezierPath(arcCenter: center,
+                                       radius: bounds.width/2 - 2.5,
+                                       startAngle: startAngle,
+                                       endAngle: outlineEndAngle,
+                                       clockwise: true)
+        
+        
+        
+        //경로 닫기
+        //outlinePath.close()
+        //그리기
+        outlineColor.setStroke()
+        outlinePath.lineWidth = 6.0
+        outlinePath.stroke()
+    }
+}
