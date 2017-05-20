f639a710bf1598664bb50412f1a5e4ce26a9d13a
torpedo87 <bestofhand@naver.com>
2017-05-20 17:37:28 +0900

    Correct MyPathView layout

--- OnTheWayMain/OnTheWayMain/MenuPageViewController.swift
+++ OnTheWayMain/OnTheWayMain/MenuPageViewController.swift
@@ -36,8 +36,7 @@ class MenuPageViewController: UIViewController, CAPSPageMenuDelegate {
             .menuItemSeparatorPercentageHeight(0.1)
         ]
 
-
-        pageMenu = CAPSPageMenu(viewControllers: controllerArray, frame: CGRect(x: 0.0, y: 0.0, width: self.view.frame.width, height: self.view.frame.height), pageMenuOptions: parameters)
+        pageMenu = CAPSPageMenu(viewControllers: controllerArray, frame: CGRect(x: 0.0, y: 20.0, width: self.view.frame.width, height: self.view.frame.height), pageMenuOptions: parameters)
         self.view.addSubview(pageMenu!.view)
 
     }
