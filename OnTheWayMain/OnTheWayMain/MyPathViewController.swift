fe711c80651eda84520565a8265bd8a9084dba1a
torpedo87 <bestofhand@naver.com>
2017-05-29 01:06:14 +0900

    Add staticMap

--- OnTheWayMain/OnTheWayMain/MyPathViewController.swift
+++ OnTheWayMain/OnTheWayMain/MyPathViewController.swift
@@ -2,67 +2,93 @@ import UIKit
 import Mapbox
 import RealmSwift
 
-
 class MyPathViewController: UIViewController, MGLMapViewDelegate {
     var calenderManager = CalenderManager()
     var realm: Realm!
     var today = String()
+    var timer: Timer?
+    var polylineSource: MGLShapeSource?
+    var currentIndex = 1
+    var allCoordinates: [CLLocationCoordinate2D]!
     
     @IBOutlet weak var mapView: MGLMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        today = calenderManager.getWeekArrStr()[5]
         mapView.delegate = self
         mapView.setUserTrackingMode(.follow, animated: true)
-        addPointsOnTheMap()
+        allCoordinates = addPointsOnTheMap()
         NotificationCenter.default.addObserver(self, selector: #selector(addPointsOnTheMap), name: Notification.Name("locationDraw"), object: nil)
     }
     
-    func mapView(_ mapView: MGLMapView, viewFor annotation: MGLAnnotation) -> MGLAnnotationView? {
-        
-        guard annotation is MGLPointAnnotation else {
-            return nil
-        }
-        
-        let reuseIdentifier = "\(annotation.coordinate.longitude)"
-        
-        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseIdentifier)
-        
-        if annotationView == nil {
-            annotationView = CustomAnnotationView(reuseIdentifier: reuseIdentifier)
-            annotationView!.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
-            
-            // Set the annotation view’s background color to a value determined by its longitude.
-            let hue = CGFloat(annotation.coordinate.longitude) / 100
-            annotationView!.backgroundColor = UIColor(hue: hue, saturation: 0.5, brightness: 1, alpha: 1)
-        }
-        
-        return annotationView
+    func mapView(_ mapView: MGLMapView, didFinishLoading style: MGLStyle) {
+        addLayer(to: style)
+        animatePolyline()
     }
     
-    func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
-        return true
+    func addLayer(to style: MGLStyle) {
+        
+        let source = MGLShapeSource(identifier: "polyline", shape: nil, options: nil)
+        style.addSource(source)
+        polylineSource = source
+        
+        let layer = MGLLineStyleLayer(identifier: "polyline", source: source)
+        layer.lineJoin = MGLStyleValue(rawValue: NSValue(mglLineJoin: .round))
+        layer.lineCap = MGLStyleValue(rawValue: NSValue(mglLineCap: .round))
+        layer.lineColor = MGLStyleValue(rawValue: UIColor.red)
+        layer.lineWidth = MGLStyleFunction(interpolationMode: .exponential,
+                                           cameraStops: [14: MGLConstantStyleValue<NSNumber>(rawValue: 5),
+                                                         18: MGLConstantStyleValue<NSNumber>(rawValue: 20)],
+                                           options: [.defaultValue : MGLConstantStyleValue<NSNumber>(rawValue: 1.5)])
+        style.addLayer(layer)
+    }
+    
+    func animatePolyline() {
+        currentIndex = 1
+        timer = Timer.scheduledTimer(timeInterval: 0.05, target: self, selector: #selector(tick), userInfo: nil, repeats: true)
+    }
+    
+    func tick() {
+        if currentIndex > allCoordinates.count {
+            timer?.invalidate()
+            timer = nil
+            return
+        }
+        
+        // Create a subarray of locations up to the current index.
+        let coordinates = Array(allCoordinates[0..<currentIndex])
+        
+        // Update our MGLShapeSource with the current locations.
+        updatePolylineWithCoordinates(coordinates: coordinates)
+        
+        currentIndex += 1
+    }
+
+    func updatePolylineWithCoordinates(coordinates: [CLLocationCoordinate2D]) {
+        var mutableCoordinates = coordinates
+        
+        let polyline = MGLPolylineFeature(coordinates: &mutableCoordinates, count: UInt(mutableCoordinates.count))
+        
+        // Updating the MGLShapeSource’s shape will have the map redraw our polyline with the current coordinates.
+        polylineSource?.shape = polyline
     }
     
     //realm에 저장된 데이터 가져와서 지도에 표시하기
-    func addPointsOnTheMap() {
+    func addPointsOnTheMap() -> [CLLocationCoordinate2D] {
         let realm = try! Realm()
         let results = realm.objects(LocationRealm.self).filter("date == '\(self.today)'")
+        var coordinates = [CLLocationCoordinate2D]()
         if results.count != 0 {
-            var pointAnnotations = [MGLPointAnnotation]()
             
-            DispatchQueue.main.async {
-                for coordinate in results {
-                    let point = MGLPointAnnotation()
-                    point.coordinate = CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude)
-                    pointAnnotations.append(point)
-                }
-                //선 그리기
-                self.mapView.addAnnotations(pointAnnotations)
+            for coordinate in results {
+                let point = CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude)
+                coordinates.append(point)
             }
             
+            
         }
-        
+        return coordinates
     }
     
     //사용자 승인시 위치추적
---
+++ OnTheWayMain/OnTheWayMain/StaticMapViewController.swift
@@ -0,0 +1,58 @@
+//
+//  StaticMapViewController.swift
+//  OnTheWayMain
+//
+//  Created by junwoo on 2017. 5. 29..
+//  Copyright © 2017년 junwoo. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import MapboxStatic
+import RealmSwift
+
+class StaticMapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: UIImageView!
+    let accessToken = "pk.eyJ1Ijoic2FtY2hvbiIsImEiOiJjajJiYzFrNXkwMGx2MzRudnNkbHptMTN5In0.aenVwqUxhFT1-bwk48svNg"
+    var calenderManager = CalenderManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let realm = try! Realm()
+        let results = realm.objects(LocationRealm.self).filter("date == '\(self.accessibilityHint!)'")
+        
+        if results.count != 0 {
+            var coordinates = [CLLocationCoordinate2D]()
+            
+            
+            for coordinate in results {
+                let point = CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude)
+                coordinates.append(point)
+            }
+            
+            let path = Path(coordinates: coordinates)
+            path.fillColor = UIColor.red.withAlphaComponent(0.5)
+            path.strokeColor = UIColor.green.withAlphaComponent(0.5)
+            
+            
+            let options = SnapshotOptions(
+                styleURL: URL(string: "mapbox://styles/mapbox/dark-v9")!,
+                size: mapView.bounds.size)
+            
+            options.overlays.append(path)
+            
+            _ = Snapshot(options: options, accessToken: accessToken).image { [weak self] (image, error) in
+                if let error = error {
+                    print(error)
+                    return
+                }
+                
+                self?.mapView.image = image
+            }
+        }
+        
+        
+    }
+}
