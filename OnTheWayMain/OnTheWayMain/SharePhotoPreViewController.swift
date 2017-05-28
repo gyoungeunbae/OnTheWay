d10d8f8717be2ff3b6c023d185c4080a3b85d520
gyoungeunbae <ruddmsdl000@naver.com>
2017-05-29 04:09:27 +0900

    Conflict resolved

--- OnTheWayMain/OnTheWayMain/SharePhotoPreViewController.swift
+++ OnTheWayMain/OnTheWayMain/SharePhotoPreViewController.swift
@@ -82,12 +82,4 @@ extension UIView {
         return img!
     }
 }
-extension UIView {
-    func snapshot() -> UIImage {
-        UIGraphicsBeginImageContextWithOptions(self.bounds.size, true, UIScreen.main.scale)
-        self.layer.render(in: UIGraphicsGetCurrentContext()!)
-        let img = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return img!
-    }
-}
+
