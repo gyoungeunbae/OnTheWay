3ce9546034993676c31cc599a75250c0d85150bf
torpedo87 <bestofhand@naver.com>
2017-05-30 17:14:27 +0900

    Add pathline on the picture

---
+++ OnTheWayMain/OnTheWayMain/PathView.swift
@@ -0,0 +1,37 @@
+//
+//  PathView.swift
+//  OnTheWayMain
+//
+//  Created by junwoo on 2017. 5. 30..
+//  Copyright © 2017년 junwoo. All rights reserved.
+//
+
+import UIKit
+
+class PathView: UIView {
+    
+    
+    override func draw(_ rect: CGRect) {
+        
+        let cgPointsArr = PointManager.sharedInstance.getTodayCGPointsArr()
+        print("point = \(cgPointsArr)")
+        for index in 0..<cgPointsArr.count-1 {
+            
+            let context = UIGraphicsGetCurrentContext()
+            
+            context?.setStrokeColor(UIColor.white.cgColor)
+            
+            context?.setLineWidth(8.0)
+            
+            context?.move(to: CGPoint(x: self.bounds.width*(cgPointsArr[index].x/375), y: self.bounds.height*(cgPointsArr[index].y)/667))
+            
+            context?.addLine(to: CGPoint(x: self.bounds.width*(cgPointsArr[index+1].x/375), y: self.bounds.height*(cgPointsArr[index+1].y)/667))
+            
+            context?.strokePath()
+            
+        }
+        
+    }
+    
+    
+}
